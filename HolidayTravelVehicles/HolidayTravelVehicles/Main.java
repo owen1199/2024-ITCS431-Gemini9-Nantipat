3fae331e9bee9a9db644641fa5ca5fd8cd6c6cc2
Weixian-victor <163420787+Weixian-victor@users.noreply.github.com>
2025-02-20 22:39:28 +0800

    Main.java

--- HolidayTravelVehicles/HolidayTravelVehicles/Main.java
+++ HolidayTravelVehicles/HolidayTravelVehicles/Main.java
@@ -1,1 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Customer customer = new Customer("C001", "John Doe", "123 Main St", "555-1234");
+        Vehicle vehicle = new Vehicle("V001", "RV Model X", "X-2024", 2024, "TravelCorp", 50000.0);
+        Invoice invoice = new Invoice("INV1001", customer, vehicle, 55000.0);
 
+        invoice.displayInvoice();
