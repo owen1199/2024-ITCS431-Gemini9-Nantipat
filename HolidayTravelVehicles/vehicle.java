e36df2e0335fa433358bd2178e6e566b8cacb18b
arelo11 <107537079+Poottapol@users.noreply.github.com>
2025-02-20 21:43:00 +0700

    Add files via upload

---
+++ HolidayTravelVehicles/vehicle.java
@@ -0,0 +1,20 @@
+public class Vehicle {
+    private String brand;
+    private String model;
+    private int year;
+    private double price;
+
+    public Vehicle(String brand, String model, int year, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+    }
+
+    public void displayInfo() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+        System.out.println("Price: $" + price);
+    }
+}
